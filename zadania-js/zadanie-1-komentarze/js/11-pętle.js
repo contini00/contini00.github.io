5bf25a8612d7e0cfa2e753ebdc8f1e588abf05e5
Wojciech Wojciechowski <wojciechowskiwojciech00@gmail.com>
2019-06-01 16:53:23 +0200

    koniec na dzis

---
+++ zadania-js/zadanie-1-komentarze/js/11-pętle.js
@@ -0,0 +1,12 @@
+
+for(i=0; i<8; i++){
+    document.write(`Obrót pętli <b> nr ${i + 1}</b>, watość zmiennej <b>"i" = ${i}</b> <br>`)
+}
+
+const imiona =['Magda', 'Gabrysia', 'Lycyfer', 'Włóczykij'];
+
+const count = imiona.length;
+
+for (let i=0; i < count; i++){
+    console.log(`Imię ${imiona[i]}--->---`);
+}
