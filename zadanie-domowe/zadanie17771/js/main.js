9b8d27997442404efbbf923ead142429b4b07ac7
Wojciech Wojciechowski <wojciechowskiwojciech00@gmail.com>
2019-06-12 20:36:36 +0200

    koniec

---
+++ zadanie-domowe/zadanie17771/js/main.js
@@ -0,0 +1,53 @@
+$(function () {
+
+let btn = $('#btn');
+
+btn.click(function() {
+    console.log('działa');
+    fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
+.then(response => response.json())
+.then(response=>{
+
+    console.log(response)
+
+    let div = document.createElement('div');
+
+    let linia = document.createElement('hr');
+    let linia2 = document.createElement('hr');
+
+    let paragraf1 = document.createElement('p');
+    let textP = document.createTextNode(`Imię: ${response.imie}`);
+
+    let paragraf2 = document.createElement('p');
+    let textP2 = document.createTextNode(`Nazwisko: ${response.nazwisko}`);
+
+    let paragraf3 = document.createElement('p');
+    let textP3 = document.createTextNode(`Zawód: ${response.zawod}`);
+
+    let paragraf4 = document.createElement('p');
+    let textP4 = document.createTextNode(`Firma: ${response.firma}`);
+
+    document.body.appendChild(div);
+    
+    div.appendChild(linia);
+
+    div.appendChild(paragraf1);
+    paragraf1.appendChild(textP);
+
+    div.appendChild(paragraf2);
+    paragraf2.appendChild(textP2);
+
+    div.appendChild(paragraf3);
+    paragraf3.appendChild(textP3);
+
+    div.appendChild(paragraf4);
+    paragraf4.appendChild(textP4);
+
+    div.appendChild(linia2);
+});
+
+});
+
+
+
+});
